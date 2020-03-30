eb684999ef840a24a0a1557753ee1e5cafa5a928
s61t6ftaro <bisk1280392@gn.iwasaki.ac.jp>
2020-03-30 13:00:05 +0900

    user validation and url access security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -13,10 +15,19 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
+    end
   end
 private
+  def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to user_path(current_user)
+    end
+  end
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
