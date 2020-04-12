b148baa570ff47dfa7c6b0d7ef1bceef14e79622
s61t6ftaro <bisk1280392@gn.iwasaki.ac.jp>
2020-04-12 21:51:17 +0900

    validation and success_message and error_messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @booknew = Book.new
+    @book = Book.new
     @books = Book.all
   end
 
@@ -12,25 +12,36 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "successfully"
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end 
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "successfully"
     redirect_to books_path
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update_attributes(book_params)
-    redirect_to book_path
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_params)
+      redirect_to book_path
+      flash[:notice] = "successfully"
+    else
+      render 'edit'
+    end
   end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
-  end 
 
 private
   def book_params
